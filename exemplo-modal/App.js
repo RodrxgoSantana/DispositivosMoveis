cbcb93c7f1fc41b9c82bcb1b8965e18d81c7a14f
Rodrigo Santana <rodrxgogit@outlook.com>
2021-08-31 21:03:31 -0300

    ADO 01

---
+++ exemplo-modal/App.js
@@ -0,0 +1,169 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, FlatList, Pressable,Modal } from 'react-native';
+import Constants from 'expo-constants';
+
+
+const ShowDetalhes = ({display,toogleModal,mensagem,outroParamentro}) => (   
+    <Modal
+          animationType="slide"
+          transparent={true}
+          visible={display}
+          onRequestClose={toogleModal}
+    >
+
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+                <Pressable onPress={toogleModal}>
+                  <Text>{mensagem + outroParamentro}</Text>
+                </Pressable>
+          </View>
+        </View>
+    
+    </Modal>
+        
+ )
+
+const Comentario = ({title,body}) => {
+
+    var exemplo = 'xpto'
+    
+    //state para controle do Modal
+    const [modal,setModal] = React.useState(false)
+
+    function mudaModal(){
+      setModal(!modal)
+    }
+
+    return(
+    <View>
+      <ShowDetalhes display={modal} toogleModal={mudaModal} mensagem={body} outroParamentro={exemplo}/>
+      
+      <Pressable onPress={mudaModal}>
+        <Text style={styles.paragraph}>{title}</Text>
+      </Pressable>
+    </View>
+    )
+}
+
+
+const DATA = [
+        {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "nesciunt quas odio",
+    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "dolorem eum magni eos aperiam quia",
+    "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "magnam facilis autem",
+    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+  }
+    ];
+
+
+
+//item com uma arrow function
+/*const meuItemObj = ({item}) => (
+  <View>
+      <Text style={styles.paragraph}>{item.title}</Text>
+    </View>
+)*/
+
+
+
+export default function App() {
+
+  //função que renderiza cada item do FlatList
+  function meuItem({item}){
+    let titleCompleto = item.title
+    
+    return(
+      <Comentario title={titleCompleto} 
+              body={item.body}
+      />
+    )
+  }
+  
+
+  return (
+
+    <View style={styles.container}>
+
+      <FlatList
+        data={DATA}
+        renderItem={meuItem}
+        keyExtractor={item => item.id}
+      />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: 'black',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 12,
+    padding: 12,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'white'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
